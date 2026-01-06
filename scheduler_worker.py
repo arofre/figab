665ca2cac7e0ddce13ab525fd6daea3854a4d21f
Aron Fredriksson <aron.fredriksson76@gmail.com>
2026-01-06 15:35:26 +0100

    Test for the scheduler

--- scheduler_worker.py
+++ scheduler_worker.py
@@ -1,6 +1,13 @@
 from app import incremental_update
 from apscheduler.schedulers.blocking import BlockingScheduler
+import datetime
 
 scheduler = BlockingScheduler()
-scheduler.add_job(incremental_update, 'cron', hour=23, minute=0)
+
+scheduler.add_job(
+    incremental_update,
+    'date',
+    run_date=datetime.datetime.now() + datetime.timedelta(seconds=10)
+)
+
 scheduler.start()
